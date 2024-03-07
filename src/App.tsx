d91f13bb1c8132e46293b6d4ca2441c3cd1c57d1
Jefferson Lambert <83100592+mediumPuppy@users.noreply.github.com>
2024-03-06 23:15:54 -0700

    first commit woo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teams from './CollegeBasketballTeams.json';
+
+function Heading() {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>College Basketball Teams</h1>
+    </div>
+  );
+}
+
+function TeamList() {
+  return (
+    <div>
+      <h2>Teams</h2>
+      <ul>
+        {teams.teams.map((team) => (
+          <li style={{ textAlign: 'left' }}>
+            {team.school} {team.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function TeamCard() {
+  return (
+    <div>
+      {teams.teams.map((team) => (
+        <div style={{ textAlign: 'left' }}>
+          <p>School: {team.school}</p>
+          <p>Mascot: {team.name}</p>
+          <p>Location: {team.city}</p>
+          <br></br>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading />
       </header>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}
+      >
+        <TeamCard />
+        <TeamList />
+      </div>
     </div>
   );
 }
